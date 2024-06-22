0cd2c6d66532b1e7199cdbbad7c65ba050fba08c
HakimMohamed <iabdelhakimmohamed@gmail.com>
2024-06-22 21:51:14 +0300

    s

--- src/app/student/login/login.component.ts
+++ src/app/student/login/login.component.ts
@@ -7,49 +7,44 @@ import { AlertifyService } from 'src/app/alertify.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   username: string | undefined;
   password: string | undefined;
   loading: boolean = false;
   loginForm!: FormGroup;
-  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router , private alertify: AlertifyService) {
-
-  }
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router,
+    private alertify: AlertifyService
+  ) {}
   ngOnInit(): void {
     if (localStorage.getItem('role') != '') {
-
-
     }
-
 
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
     });
   }
   Onlogin() {
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
       this.auth.Studentlogin(this.loginForm.value).subscribe(
-        next => {
+        (next) => {
           console.log('تم الدخول');
-          
-            this.alertify.success('Welcome ' + this.loginForm.value.username)
-            this.router.navigate(['student/home/student-course']);
- 
 
+          this.alertify.success('Welcome ' + this.loginForm.value.username);
+          this.router.navigate(['student/home/main']);
         },
-        error => {
-          this.alertify.error('Incorrect Username or Password.')
+        (error) => {
+          this.alertify.error('Incorrect Username or Password.');
           console.log('فشل في الدخول');
-
         }
-
-      )
-    }
-    else {
+      );
+    } else {
       console.log('form is not valid');
     }
   }
