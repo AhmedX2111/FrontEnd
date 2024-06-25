b46bcb36c8ddc292e8d4938ef08067f1e6c9009a
HakimMohamed <iabdelhakimmohamed@gmail.com>
2024-06-25 03:11:31 +0300

    aspdlasdlkmasd

--- src/app/course.service.ts
+++ src/app/course.service.ts
@@ -3,48 +3,70 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CourseService {
-
-
   private apiUrl = 'https://localhost:5001/api/';
 
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getAllCourses(): Observable<any> {
-
-    return this.http.get<any>(this.apiUrl + "Courses");
+    return this.http.get<any>(this.apiUrl + 'Courses');
   }
   getCourses(): Observable<any> {
-
-    const user_Id = localStorage.getItem("user_Id");
+    const user_Id = localStorage.getItem('user_Id');
     console.log(user_Id);
-    return this.http.get<any>(this.apiUrl + "Students/GetCoursesByStudent/" +user_Id);
+    return this.http.get<any>(
+      this.apiUrl + 'Students/GetCoursesByStudent/' + user_Id
+    );
   }
-  getCourseNameByCode(coursecode:any){
-    return this.http.get<any>(this.apiUrl + "Students/GetCourseNameByCode/" +coursecode);
+  getCourseNameByCode(coursecode: any) {
+    return this.http.get<any>(
+      this.apiUrl + 'Students/GetCourseNameByCode/' + coursecode
+    );
   }
-  ConfirmCourseByCode(coursecode:any,studentid:any)
-  {
-    return this.http.post<any>(this.apiUrl + "Students/ConfirmCourseByCode/" +coursecode+"/"+studentid,null);
+  ConfirmCourseByCode(coursecode: any, studentid: any) {
+    return this.http.post<any>(
+      this.apiUrl +
+        'Students/ConfirmCourseByCode/' +
+        coursecode +
+        '/' +
+        studentid,
+      null
+    );
   }
-  addCourse(course:any)
-  {
-    return this.http.post<any>(this.apiUrl + "Courses/",course);
+  addCourse(course: any) {
+    return this.http.post<any>(this.apiUrl + 'Courses/', course);
   }
-   deleteCourse(courseId: any): Observable<any> {
-     return this.http.delete<any>(`${this.apiUrl}Courses/${courseId}`);
-   }
-  
+  deleteCourse(courseId: any): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}Courses/${courseId}`);
+  }
+
   getUploadsByCourseId(courseId: number): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl+`Uploads/GetUploadsByCourseID/${courseId}`);
+    return this.http.get<any[]>(
+      this.apiUrl + `Uploads/GetUploadsByCourseID/${courseId}`
+    );
   }
   getStudentsByCourseId(courseId: number): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl+`Courses/GetAllStudentsByCourseId/${courseId}`);
+    return this.http.get<any[]>(
+      this.apiUrl + `Courses/GetAllStudentsByCourseId/${courseId}`
+    );
   }
-  updateGrade(updateGradeDto: { studentId: number, courseId: number, grade: number }): Observable<void> {
-    return this.http.put<void>(`${this.apiUrl}Courses/UpdateGrade`, updateGradeDto);
+  updateGrade(updateGradeDto: {
+    studentId: number;
+    courseId: number;
+    grade: number;
+  }): Observable<void> {
+    return this.http.put<void>(
+      `${this.apiUrl}Courses/UpdateGrade`,
+      updateGradeDto
+    );
+  }
+  postMatertial(courseId: any, fileUrl: any, uploadName: any) {
+    return this.http.post<any>(`${this.apiUrl}Uploads`, {
+      courseId,
+      fileUrl,
+      uploadName,
+    });
   }
 }
--- src/app/doctor/course-uploads/course-uploads.component.ts
+++ src/app/doctor/course-uploads/course-uploads.component.ts
@@ -4,37 +4,48 @@ import { CourseService } from 'src/app/course.service';
 @Component({
   selector: 'app-course-uploads',
   templateUrl: './course-uploads.component.html',
-  styleUrls: ['./course-uploads.component.css']
+  styleUrls: ['./course-uploads.component.css'],
 })
 export class CourseUploadsComponent {
-  courses: any[] = []; // Array to store courses
-  uploads: any[] = []; // Array to store uploads
+  courses: any[] = [];
+  uploads: any[] = [];
   selectedCourseId!: number;
+  uploadName: any;
+  fileUrl: any;
   constructor(private service: CourseService) {
     this.loadCourses();
   }
 
   loadCourses() {
-    this.service.getAllCourses().subscribe(courses => {
+    this.service.getAllCourses().subscribe((courses) => {
       this.courses = courses;
-      console.log(courses);
     });
   }
 
+  postMatertial() {
+    console.log(this.selectedCourseId, this.fileUrl, this.uploadName);
+    this.service
+      .postMatertial(this.selectedCourseId, this.fileUrl, this.uploadName)
+      .subscribe((res) => {
+        console.log(res);
+        this.service
+          .getUploadsByCourseId(this.selectedCourseId)
+          .subscribe((uploads) => {
+            this.uploads = uploads;
+          });
+      });
+  }
   onCourseSelect() {
     if (this.selectedCourseId) {
-      this.service.getUploadsByCourseId(this.selectedCourseId)
-        .subscribe(uploads =>
-          {
-            this.uploads = uploads;
-            console.log(this.uploads);
-          } 
-
-
-
-        );
+      this.service
+        .getUploadsByCourseId(this.selectedCourseId)
+        .subscribe((uploads) => {
+          console.log('course');
+          console.log(this.uploads);
+          this.uploads = uploads;
+        });
     } else {
-      this.uploads = []; // Clear uploads if no course selected
+      this.uploads = [];
     }
   }
 }
